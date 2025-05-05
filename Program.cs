f2f441258b6486000949606b3ef130ab218baf01
Gabrielalves5 <Gabrielalves5>
2025-05-05 09:57:59 -0300

    Horas extras

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿double salario;
+int hora, horaEXtra;
+    Console.WriteLine("Digite seu salario por hora..: ");
+       salario = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Digite a quantidade de horas trabalhadas..: ");
+        hora = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Digite a quantidade de horas extras trabalhadas..: ");
+        horaEXtra = Convert.ToInt32(Console.ReadLine());
+double salarioExtra = salario *0.40;
+
+    salarioExtra = salarioExtra*horaEXtra;
+    salario = salario*hora;
+    Console.WriteLine($"Seu salario é {salario+salarioExtra}");
+
+
